6c3dd37090ddd774fe5b11f352aa93436830bebb
David Morrow <quickbasic44@outlook.com>
2022-06-14 17:39:54 -0400

    Add project files.

---
+++ BindableProperties/BindableProperties/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace BindableProperties
+{
+    public partial class MainPage : ContentPage
+    {
+        public string MyDescription { get; set; } = "Did you subscribe yet?";
+
+        public MainPage()
+        {
+            InitializeComponent();
+            MyControl.ChildAdded += MyControl_ChildAdded;
+            MyControl.DescendantAdded += MyControl_DescendantAdded;
+            BindingContext = this;
+        }
+
+        private void MyControl_DescendantAdded(object sender, ElementEventArgs e)
+        {
+            DisplayAlert("Descendant Added", e.Element.ToString(), "cancel");
+        }
+
+        private void MyControl_ChildAdded(object sender, ElementEventArgs e)
+        {
+            DisplayAlert("Child Added", e.Element.ToString(), "cancel");
+        }
+    }
+}
